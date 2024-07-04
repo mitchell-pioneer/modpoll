9e28c6aae4945f49a48346f1052f722f7ade86c9
mitchell-pioneer <mitchell@pioneer-emobility.com>
2024-07-04 11:43:42 -0400

    updates for run-as-a-service

--- contrib/run-as-a-service/run-modpoll.sh
+++ contrib/run-as-a-service/run-modpoll.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
-cd /home/mbalsam/modpoll
+cd /opt/modpoll
 source venv/bin/activate
-python3 main.py --tcp 10.1.1.88 -f tristar.csv --mqtt-host devmqtt.eboostmobile.com --mqtt-single 
+python3 main.py \
+ --tcp 10.1.1.88 \
+  -f tristar-test.csv \
+  --mqtt-host devmqtt.eboostmobile.com \
+  --mqtt-single \
+  --mqtt-onchange \
+  --delay 5 \
+  --daemon \
+  --mqtt-topic "pioneer/modbus/customer/product/" 
+
+
 
--- run-modpoll.sh
+++ run-modpoll.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-cd /home/mbalsam/modpoll
+cd /opt/modpoll
 source venv/bin/activate
 python3 main.py \
  --tcp 10.1.1.88 \
